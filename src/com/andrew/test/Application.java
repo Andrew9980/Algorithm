ac53640f3370f410491a7cf16ce570d5f76f042f
Andrew9980 <2430209342@qq.com>
2018-09-11 11:29:39 +0800

    创建项目

---
+++ src/com/andrew/test/Application.java
@@ -0,0 +1,16 @@
+package com.andrew.test;
+
+/**
+ * @author Andrew
+ * @Title: Application
+ * @ProjectName Algorithm
+ * @Description: TODO
+ * @date 2018/9/1111:26
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Algorithm");
+    }
+
+}
